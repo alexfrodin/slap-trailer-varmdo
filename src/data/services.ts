80af91a64505e62ce48caa095062b67070216554
alexfrodin <73692451+alexfrodin@users.noreply.github.com>
2026-09-20 19:15:14 +0200

    fix textes

--- src/data/services.ts
+++ src/data/services.ts
@@ -149,7 +149,7 @@ export const services: Service[] = [
       whatWeDoTitle: 'Vad vi gör',
       whatWeDo: [
         'Felsöker påskjutsbroms och justerar när det går',
-        'Byter bromsvajrar, backar och andra slitdelar',
+        'Byter bromsvajrar, bromsbackar och andra slitna delar',
         'Ser över parkeringsbroms',
         'Kontrollerar att hjulen rullar fritt efter justering',
         'Kombinerar gärna med lagerkontroll om släpet ändå är uppe',
